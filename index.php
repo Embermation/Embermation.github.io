ad45f20fc536e7674d6ba3c5b10f47018b3276d0
Embermation <101485722+Embermation@users.noreply.github.com>
2023-10-20 08:08:14 -0400

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<a href=https://embermation.github.io/overflow.html>overflow</a>
-<a href=https://embermation.github.io/wrong.html>somethign went wrong</a>
-<a href=https://embermation.github.io/shutdown.html>shutdown</a>
-<a href=https://embermation.github.io/sketch_whitespace.html>sketchbook</a>
-<a href=https://embermation.github.io/blackspace.html>sketchbook blackspace</a>
-<a href=https://embermation.github.io/maintenance.html>maintenance</a>
